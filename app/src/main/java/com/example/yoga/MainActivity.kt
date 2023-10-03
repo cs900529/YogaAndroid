54b1df9a4042fe1a7340b86536fb6b31c89ae447
James Lyu <seer.28921085@gmail.com>
2023-10-03 22:28:58 +0800

    CalibrationView (unfinish)

--- app/src/main/java/com/example/yoga/MainActivity.kt
+++ app/src/main/java/com/example/yoga/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.yoga
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide() // 隐藏title bar
         setContentView(R.layout.activity_main)
+
+        val start = findViewById<Button>(R.id.start)
+        start.setOnClickListener {
+            // 頁面跳轉
+            val intent = Intent(this, CalibrationStage::class.java)
+            startActivity(intent)
+        }
+
     }
 }
