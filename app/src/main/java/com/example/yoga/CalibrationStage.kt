424ee569779d18ecc92a173264f46b4a90175ed0
28921085 <seer.28921085@gmail.com>
2023-10-04 18:10:17 +0800

    Calibration view without camera

--- app/src/main/java/com/example/yoga/CalibrationStage.kt
+++ app/src/main/java/com/example/yoga/CalibrationStage.kt
@@ -1,9 +1,9 @@
 package com.example.yoga
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
-import android.widget.LinearLayout.LayoutParams
 
 class CalibrationStage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,20 +12,13 @@ class CalibrationStage : AppCompatActivity() {
         setContentView(R.layout.activity_calibration_stage)
 
         val finish_button = findViewById<ImageButton>(R.id.finish)
+        finish_button.setOnClickListener {
+            // 頁面跳轉
+            val intent = Intent(this, Menu::class.java)
+            startActivity(intent)
+        }
 
-        // 获取屏幕宽度和高度
-        val displayMetrics = resources.displayMetrics
-        val screenWidth = displayMetrics.widthPixels
-        val screenHeight = displayMetrics.heightPixels
 
-        // 计算按钮的宽度和高度为屏幕宽度和高度的10%
-        val buttonWidth = (0.1 * screenWidth).toInt()
-        val buttonHeight = (0.1 * screenHeight).toInt()
 
-        // 设置按钮的布局参数
-        val layoutParams = LayoutParams(buttonHeight, buttonHeight)
-        layoutParams.topMargin=(0.9*screenWidth).toInt()
-        layoutParams.leftMargin=(0.9*screenHeight).toInt()
-        finish_button.layoutParams = layoutParams
     }
 }
---
+++ app/src/main/java/com/example/yoga/Menu.kt
@@ -0,0 +1,12 @@
+package com.example.yoga
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+        supportActionBar?.hide() // 隐藏title bar
+    }
+}
