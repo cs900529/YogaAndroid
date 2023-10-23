4b22497d7b93a165752b0529a48e0c1613338a02
James Lyu <seer.28921085@gmail.com>
2023-10-24 01:05:51 +0800

    menu view layout finish (but content unfinish

--- app/src/main/java/com/example/yoga/Menu.kt
+++ app/src/main/java/com/example/yoga/Menu.kt
@@ -1,12 +1,20 @@
 package com.example.yoga
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class Menu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
         supportActionBar?.hide() // 隐藏title bar
+        val back_button = findViewById<ImageButton>(R.id.back)
+        back_button.setOnClickListener {
+            // 頁面跳轉
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
