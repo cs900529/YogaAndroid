25a64bd77ea18de897d5c63b522fad9692c6961d
Cobra 陳俊宇 <44718953+Cobra30621@users.noreply.github.com>
2024-01-10 14:41:34 +0800

    fix: point transform problem

--- app/src/main/python/YogaMatProcessor.py
+++ app/src/main/python/YogaMatProcessor.py
@@ -67,7 +67,7 @@ def check_raise_feet(point3d):
     }
 
     raise_feet_interval = 0.1
-    print("left_point[1]", left_point[1], "right_point[1]", right_point[1])
+#     print("left_point[1]", left_point[1], "right_point[1]", right_point[1])
 
     # 如果兩者的 y 座標差距大於 raise_feet_interval，則更新 raise_feet_dict
     if abs(left_point[1] - right_point[1]) > raise_feet_interval:
@@ -91,10 +91,10 @@ class YogaMatProcessor:
         # 鏡頭的長寬
         self.camera_flat_points = np.array([[1, 1], [0, 1], [0, 0], [1, 0]], dtype=np.float32)
         # 預設瑜珈墊在鏡頭的座標
-        default_mat_point = np.array([[0.8 , 0.97 ],
-                                      [0.2 , 0.97 ],
-                                      [0.25 , 0.76 ],
-                                      [0.75 , 0.76 ]], dtype=np.float32)
+        default_mat_point = np.array([[0.9 , 0.97 ],
+                                      [0.1 , 0.97 ],
+                                      [0.15 , 0.76 ],
+                                      [0.85 , 0.76 ]], dtype=np.float32)
 
         print(default_mat_point)
         # 轉換矩陣
@@ -102,9 +102,6 @@ class YogaMatProcessor:
 
     # 最終取得腳的方法
     def get_feet_data(self, r_point2d, r_point3d, center):
-
-        print("r_point3d", r_point3d)
-        print("r_point2d", r_point2d)
         point3d = []
         for i in range(r_point3d.size()):
             ang = []
@@ -143,9 +140,6 @@ class YogaMatProcessor:
                 AngleNodeDef.RIGHT_HEEL: (transform_points[1][0], transform_points[1][1]),
             }
 
-            # print("raise_feet_dict", raise_feet_dict)
-            print("feet_points_dict", feet_points_dict)
-
 #             for key, feet_on_mat in raise_feet_dict.items():
 #                 # 如果抬腳，較刪除該點
 #                 if not feet_on_mat:
@@ -176,6 +170,7 @@ class YogaMatProcessor:
 
     # 將鏡頭中的座標點，轉換到瑜珈墊上面
     def transform_point(self, input_points):
+        print("feet self.transform_matrix", self.transform_matrix)
         transformed_points = [cv2.perspectiveTransform(np.array([np.float32([p])]), self.transform_matrix)[0][0] for p
                               in input_points]
 
@@ -185,7 +180,7 @@ class YogaMatProcessor:
     # 從 MediaPipe 的點中，取得在鏡頭中腳的點
     def get_feet_points(self, point2d):
         if not isinstance(point2d, int):
-            print("feet get_feet_points L: " + str(point2d[AngleNodeDef.LEFT_HEEL]) + "R:" + str(point2d[AngleNodeDef.RIGHT_HEEL]))
+#             print("feet get_feet_points L: " + str(point2d[AngleNodeDef.LEFT_HEEL]) + "R:" + str(point2d[AngleNodeDef.RIGHT_HEEL]))
             return [[point2d[AngleNodeDef.LEFT_HEEL][0] , point2d[AngleNodeDef.LEFT_HEEL][1] ],
             [point2d[AngleNodeDef.RIGHT_HEEL][0] , point2d[AngleNodeDef.RIGHT_HEEL][1] ]]
         else:
